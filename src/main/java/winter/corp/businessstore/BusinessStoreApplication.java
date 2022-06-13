1c93671f4cd41afcb2ee99092c24402e3d037cc9
dmitriy <vdmitriy003@gmail.com>
2022-06-14 00:48:05 +0300

    Initial

---
+++ src/main/java/winter/corp/businessstore/BusinessStoreApplication.java
@@ -0,0 +1,13 @@
+package winter.corp.businessstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/winter/corp/businessstore/BusinessStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package winter.corp.businessstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
